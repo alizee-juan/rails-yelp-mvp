8a3b7fadac2c43876f775f982301d004e0ef28e9
alizee juan <alizee.juan@kedgebs.com>
2021-05-26 16:23:48 +0200

    exo2

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, :rating, presence: true
+  validates_numericality_of :rating
+  validates_inclusion_of :rating, :in => 0..5
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "+33 666 999 000", category: "belgian" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "+33 777 888 000", category: "italian"  }
+pho_14 =  { name: "Pho 14", address: "92 E, Impasse des Cigales, 13360 Roquevaire", phone_number: "+33 777 666 000", category: "japanese"  }
+ratatouille =  { name: "Chez Ratatouille", address: "169 rue Paradis, 13006 Marseille", phone_number: "+33 777 666 000", category: "french"  }
+bo_bun =  { name: "Bo Bun", address: "Avenue des frites, 04821 Aubagne", phone_number: "+33 777 555 000", category: "chinese"  }
+
+
+[ dishoom, pizza_east, pho_14, ratatouille, bo_bun].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
